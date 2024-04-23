152d5b5f5255498123af16863861e21a0dae0039
Q7Elite <karichuflora2016@gmail.com>
2024-04-23 13:57:53 +0300

    fix index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,7 +26,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 app.listen(7000, () => {
